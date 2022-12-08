ddcbca6ab6d091a14b8eab85c192bf226fda88cc
Ajay-Kandare <ajayinternship18@gmail.com>
2022-12-09 01:11:47 +0530

    first commit

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main(){
+    return (
+      <main>
+        <h1> Things i learned so far in react </h1>
+        <ul>
+            <li>creating my react app </li>
+            <li> making custom components</li>
+            <li> using import and exports</li>
+            <li> using jsx syntax</li>
+        </ul>
+      </main>
+
+
+    )
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import reactLogo from "../reactlogo.png"
+export default function Navbar(){
+    return (
+    <nav>
+        <img src={reactLogo} alt="reactlogo"/>
+        <h3> React project using custom components</h3>
+        <h4> React project-1</h4>
+
+
+    </nav>
+
+    )
+}
